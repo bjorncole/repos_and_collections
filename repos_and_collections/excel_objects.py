45a36c0f6a5a4ff10057979cdd535e7971fc3911
Bjorn Cole <blank@fake.com>
2017-10-26 07:28:01 -0400

    Figured out proper counting of rows and cols from composition tree.

--- repos_and_collections/excel_objects.py
+++ repos_and_collections/excel_objects.py
@@ -2,8 +2,8 @@
 
 import xlwings as xl
 import inspect
-import networkx as nw
-from collections import Collection
+import networkx as nx
+from repos_and_collections.collections import Collection
 
 class ExcelCollectionSet(object):
     '''
@@ -33,7 +33,69 @@ class ExcelCollection(Collection):
         # if true, put graph on first columns of sheet, if false put graph
         # on first rows of sheet
         self.row_headers = False
+        self.graphs = []
+        self.roots = []
 
+    def add_graph(self, incoming_graph, incoming_root):
+        new_graph = ExcelCellPath()
+        new_graph.path_graph = incoming_graph
+        self.graphs.append(new_graph)
+        self.roots.append(incoming_root)
+
+    def set_cells(self):
+        total_max_depth = 0
+        total_width = 0
+        for indx, grph in enumerate(self.graphs):
+            # get max depth and width for a given graph
+
+            leaf_list = []
+
+            for key, val in nx.out_degree_centrality(grph.path_graph).items():
+                if val == 0.0:
+                    leaf_list.append(key)
+
+            # assume row-wise to start; can work transpose later
+
+            cell_dict = {}
+
+            path_dict = {}
+
+            for leaf in leaf_list:
+                for tree_path in list(nx.all_shortest_paths(grph.path_graph, self.roots[0], leaf)):
+                    path_builder = ''
+                    for indx, step in enumerate(tree_path):
+                        if indx == 0:
+                            path_builder = step
+                        else:
+                            path_builder = path_builder + '.' + step
+
+                    path_dict.update({path_builder: tree_path})
+
+            col_counter = 1
+
+            for path in sorted(path_dict):
+                print(path)
+                if total_max_depth < len(path_dict[path]):
+                    total_max_depth = len(path_dict[path])
+                row_counter = len(path_dict[path])
+                for indx2, step in enumerate(reversed(path_dict[path])):
+
+                    if step in cell_dict:
+                        current_list = cell_dict[step]
+                        current_list.append([row_counter, col_counter])
+                    else:
+                        cell_dict.update({step: [[row_counter, col_counter]]})
+
+                    row_counter = row_counter - 1
+
+                col_counter = col_counter + 1
+
+            total_width = col_counter - 1
+
+            print('Max depth on cells = ' + repr(total_max_depth))
+            print('Width of cells = ' + repr(total_width))
+
+            print(repr(cell_dict))
 
 class ExcelCellPath(object):
     '''
@@ -49,8 +111,6 @@ class ExcelCellPath(object):
         :param obj: Python object to analyze
         :return: None (modifies internal state to have all paths accounted for)
         '''
-
-
 
     def add_attributes_to_graph(self, obj):
         '''
--- tests/probe_test_excel.py
+++ tests/probe_test_excel.py
@@ -2,6 +2,7 @@ from repos_and_collections.excel_objects import ExcelCollectionSet
 from pprint import PrettyPrinter
 from deep_metamodeling.metamodels.m2_uml import Class, Property, Association, AssociationClass, CompositionGraph
 from repos_and_collections.collections import Collection
+from repos_and_collections.excel_objects import ExcelCollection
 import networkx as nx
 
 def main():
@@ -19,16 +20,22 @@ def main():
 
     pp = PrettyPrinter(indent=2)
 
-    new_sheet = Collection()
+    new_sheet = ExcelCollection()
     new_sheet.name = 'Car Data'
 
     # Make up some fake UML data
 
     class1 = Class()
     class2 = Class()
+    class3 = Class()
+    class4 = Class()
+    class5 = Class()
 
     class1.name = 'Car'
     class2.name = 'Wheel'
+    class3.name = 'Hub'
+    class4.name = 'Tire'
+    class5.name = 'Belt'
 
     prop1 = Property()
     prop2 = Property()
@@ -36,6 +43,9 @@ def main():
     prop4 = Property()
     prop5 = Property()
     prop6 = Property()
+    prop7 = Property()
+    prop8 = Property()
+    prop9 = Property()
 
     prop1.name = 'front right'
     prop2.name = 'front left'
@@ -43,6 +53,9 @@ def main():
     prop4.name = 'rear left'
     prop5.name = 'diameter'
     prop6.name = 'radius'
+    prop7.name = 'hub'
+    prop8.name = 'tire'
+    prop9.name = 'belt'
 
     class1.owned_attribute.append(prop1)
     class1.owned_attribute.append(prop2)
@@ -50,11 +63,20 @@ def main():
     class1.owned_attribute.append(prop4)
 
     class2.owned_attribute.append(prop5)
+    class2.owned_attribute.append(prop6)
+    class2.owned_attribute.append(prop7)
+    class2.owned_attribute.append(prop8)
+
+    class4.owned_attribute.append(prop9)
 
     prop1.type_.append(class2)
     prop2.type_.append(class2)
     prop3.type_.append(class2)
     prop4.type_.append(class2)
+
+    prop7.type_.append(class3)
+    prop8.type_.append(class4)
+    prop4.type_.append(class5)
 
     test_graph = CompositionGraph(class1)
 
@@ -63,6 +85,31 @@ def main():
     pp.pprint(list(nx.topological_sort(test_graph.comp_graph)))
 
     pp.pprint(nx.out_degree_centrality(test_graph.comp_graph))
+
+    leaf_list = []
+
+    for key, val in nx.out_degree_centrality(test_graph.comp_graph).items():
+        if val == 0.0:
+            leaf_list.append(key)
+
+    path_dict = {}
+
+    for leaf in leaf_list:
+        for tree_path in list(nx.all_shortest_paths(test_graph.comp_graph, class1.name, leaf)):
+            path_builder = ''
+            for indx, step in enumerate(tree_path):
+                if indx == 0:
+                    path_builder = step
+                else:
+                    path_builder = path_builder + '.' + step
+
+            path_dict.update({path_builder: tree_path})
+
+    pp.pprint(path_dict)
+
+    new_sheet.add_graph(test_graph.comp_graph, class1.name)
+
+    new_sheet.set_cells()
 
     #for child in root:
     #    split_tag = etree.QName(child.tag)
